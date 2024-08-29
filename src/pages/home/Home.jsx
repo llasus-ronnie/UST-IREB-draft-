f06c9abc905a16187f53555227762750bce9ea6e
Moonie <iellemizuki@gmail.com>
2024-08-29 15:17:24 +0800

    responsiveness for sidenav

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -38,8 +38,8 @@ export default function Home() {
                 <div className='section-1'>
                     <div className='section-1-content'>
                         <h1>Welcome!</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br />
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
                         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
 
                         <div className='buttons'>
